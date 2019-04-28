366e672c520eb6e1c3c0ff1abb33e61eaf690da9
billsedison <billsedison@163.com>
2019-04-28 12:26:59 +0800

    Add testcase 46

---
+++ testcases-src/tc46-not-support-TransitionEvent/src/app/app.component.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { expect } from 'chai';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    function foo() {
+      return new TransitionEvent('transitionend');
+    }
+    expect(foo).to.not.throw();
+  });
+});
